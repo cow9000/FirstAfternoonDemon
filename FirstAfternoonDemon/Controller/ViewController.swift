3a15b183d6480f4d3346730198a7097156f7174e
Derek Vawdrey <flyingpiechicken@gmail.com>
2017-10-06 13:37:40 -0600

    Adjusted project structure to reflect MVC

--- FirstAfternoonDemon/Controller/ViewController.swift
+++ FirstAfternoonDemon/Controller/ViewController.swift
@@ -17,7 +17,20 @@ class ViewController: UIViewController {
         }else{
             sender.backgroundColor = .orange
         }
+        view.backgroundColor = createRandomColor()
     }
+    
+    private func createRandomColor() -> UIColor{
+        let newColor : UIColor
+        let redValue : CGFloat = CGFloat(Double (arc4random_uniform(256))/255.00)
+        let greenValue : CGFloat = CGFloat(Double (arc4random_uniform(256))/255.00)
+        let blueValue : CGFloat = CGFloat(Double (arc4random_uniform(256))/255.00)
+        
+        newColor = UIColor(red:redValue, green:greenValue, blue:blueValue, alpha:CGFloat(1.0))
+        
+        return newColor
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
