2ca0aa85e8a2b582fe0e79758563c88ceaf0a891
Derek Vawdrey <flyingpiechicken@gmail.com>
2017-10-18 12:17:37 -0600

    Added another view controller

---
+++ FirstAfternoonDemon/Controller/MediaController.swift
@@ -0,0 +1,42 @@
+//
+//  MediaController.swift
+//  FirstAfternoonDemon
+//
+//  Created by Vawdrey, Derek on 10/18/17.
+//  Copyright © 2017 Vawdrey, Derek. All rights reserved.
+//
+
+import UIKit
+
+class MediaController: UIViewController {
+
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var slider1: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
